cb2200bcd52cb32811c4a839629c9740421d8658
Andrea Coi <andrea.coi@outlook.it>
2025-04-13 12:31:54 +0200

    create header file in order to check if keyboard is present

---
+++ check-device.h
@@ -0,0 +1,10 @@
+#ifndef CHECK_DEVICE_H
+#define CHECK_DEVICE_H
+
+#include <stdint.h>
+
+typedef struct {
+  uint16_t vendor_id;
+  uint16_t product_id;
+} KeyboardIdentifier;
+#endif // ! CHECK_DEV
