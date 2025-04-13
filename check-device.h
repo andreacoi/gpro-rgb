2d7c2f9d684bc6bdb604b806aaf02fe4e70fb8b0
Andrea Coi <andrea.coi@outlook.it>
2025-04-13 21:46:30 +0200

    declare the constant using the extern const INSTEAD of #define
    results: max control on each type

--- check-device.h
+++ check-device.h
@@ -4,13 +4,13 @@
 #include <libusb-1.0/libusb.h>
 #include <stdint.h>
 // defines all the constants for the keyboard
-#define LOGITECH_VENDOR_ID 0x46d;
-#define LOGITECH_PRDOCUCT_ID 0xfff; // exaample product id
+extern const uint16_t VENDOR_ID;
+extern const uint16_t PRODUCT_ID;
 
 // declare struct as a KeyboardIdentifier
 typedef struct {
-  uint16_t vendor_id;
   uint16_t product_id;
+  uint16_t vendor_id;
 } KeyboardIdentifier;
 
 int check_device(libusb_device_handle *dev, KeyboardIdentifier *keyboard_id);
