5814441b79e312692b62781cefc0f7d63413b841
Andrea Coi <andrea.coi@outlook.it>
2025-04-13 21:13:45 +0200

    sets the constant VID and PID for the LOGITECH KEYBOARD - n. to be changed

--- check-device.h
+++ check-device.h
@@ -3,9 +3,15 @@
 
 #include <libusb-1.0/libusb.h>
 #include <stdint.h>
+// defines all the constants for the keyboard
+#define LOGITECH_VENDOR_ID 0x46d;
+#define LOGITECH_PRDOCUCT_ID 0xfff; // exaample product id
 
+// declare struct as a KeyboardIdentifier
 typedef struct {
   uint16_t vendor_id;
   uint16_t product_id;
 } KeyboardIdentifier;
+
+int check_device(libusb_device_handle *dev, KeyboardIdentifier *keyboard_id);
 #endif // ! CHECK_DEV
