cd92185f0a5830936cbe8aa57e14b724e7b1d251
Andrea Coi <andrea.coi@outlook.it>
2025-04-20 12:11:14 +0200

    add files - porting of the drivers written in cpp for the logitech gpro keyboard

---
+++ controller/drivers/LogitechGProKeyboardController.h
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------*\
+| LogitechGProController.h                                  |
+|                                                           |
+|   Driver for Logitech G Pro keyboard                      |
+|                                                           |
+|   andreacoi                                   20 Apr 2025 |
+|                                                           |
+|   This file is part of the GPRO-RGB Project               |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
---
+++ controller/drivers/RGBController_LogitechGProKeyboard.h
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------*\
+| RGBController_LogitechGPro.h                              |
+|                                                           |
+|   RGBController for Logitech G Pro keyboard               |
+|                                                           |
+|   andreacoi                                   20 Apr 2025 |
+|                                                           |
+|   This file is part of the GPRO-RGB  |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
